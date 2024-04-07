77b305f1fee5697d58b2ed3aec56c350c11556e9
Israel Aguilar <iaguilaror@gmail.com>
2024-04-07 13:04:17 -0600

    resumir perdida SNI

--- 01-clean.R
+++ 01-clean.R
@@ -13,3 +13,18 @@ dir.create( path = "results" )
 
 # guardar esta data
 write.csv( x = data_filtrada, file = "results/limpia.csv" )
+
+# Calcular la proporcion de Dr y DRA que pierden SNI
+data_resumen <- data_filtrada %>% 
+  filter( cambio == "perdio SNI" ) %>% 
+  group_by( nobilis ) %>% 
+  summarise( totales = sum( miembros ) )
+
+# Guardamos la data resumen
+write.csv( x = data_resumen, file = "results/resumen.csv" )
+
+# TO-DOs
+# iaguilar: Calcular el % de DR y DRA que pierde SNI
+#           como TOTAL INGRESOS / TOTAL PERDIDAS
+
+
